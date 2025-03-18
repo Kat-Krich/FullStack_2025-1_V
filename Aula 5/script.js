2929b869f99b1d09fd3e82b965b1e1f3f3e3a23b
Kat-Krich <katarina.krich@gmail.com>
2025-03-18 17:05:22 -0300

    .

---
+++ Aula 5/script.js
@@ -0,0 +1,37 @@
+// window.alert("Hello world!")
+console.log("Hello world!")
+console.log("Otorrinolaringonogista")
+
+// let nome = prompt("qual o seu nome?");
+// console.log(nome);
+
+//let i = 0;
+//while(i < 10){
+//    console.log(i);
+//   i++;
+//}
+//for(let i = 0; i < 10; i++){
+//   console.log(i)
+//}
+
+//console.log("Exercício 1")
+//for(let i = 1; i < 100; i +=2){
+//    console.log(i)
+//}
+
+//console.log("Exercício 2")
+//for(let i = 5; i < 500; i +=5){
+//    console.log(i)
+//}
+
+//let i = parseInt(prompt("Digite um numero:"))
+//while(i > 0){                   // for(let i = x; i >= 0; i--){
+//    console.log(i);             //console.log(i)
+//    i--;                        //}
+//}
+
+let y = parseInt(prompt("Digite um numero:"))
+for(let i = y; i > 0; i--){
+    y *= i;
+    console.log(i, y)
+}
