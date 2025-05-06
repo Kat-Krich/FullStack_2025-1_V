56d1cda176c2d5a6f7b3a1f1dfea6bad9c371cf5
Kat-Krich <katarina.krich@gmail.com>
2025-05-06 15:17:38 -0300

    Mudanças

--- Servidor/servidor.js
+++ Servidor/servidor.js
@@ -1,11 +1,99 @@
-// require("colors");
+// // require("colors");
+// var http = require("http");
+// var express = require("express");
+
+// var app = express();
+// app.use(express.static('./public'))
+
+// var server = http.createServer(app);
+// server.listen(81);
+
+// console.log('Servidor Rodando!')
+
+require("colors");
 var http = require("http");
 var express = require("express");
+var bodyParser = require("body-parser")
+var mongodb = require("mongodb");
+
+const MongoClient = mongodb.MongoClient;
+const uri = `mongodb+srv://fagner:KhMX80NtBAtJJKCD@aulabd.cjihvk4.mongodb.net/?retryWrites=true&w=majority&appName=AulaBD`;
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+var dbo = client.db("exemplo_bd");
+var usuarios = dbo.collection("usuarios");
 
 var app = express();
 app.use(express.static('./public'))
+app.use(bodyParser.urlencoded({extended: false }))
+app.use(bodyParser.json())
+app.set('view engine', 'ejs')
+app.set('views', './views');
 
 var server = http.createServer(app);
-server.listen(81);
+server.listen(80);
 
-console.log('Servidor Rodando!')
+console.log("Servidor rodando ...".rainbow);
+
+
+// Métodos e actions
+
+app.get("/inicio", function(requisicao, resposta){
+    resposta.redirect("Aula_1/index.html")
+})
+
+app.post("/inicio", function(requisicao, resposta){
+    resposta.redirect("Aula_1/index.html")
+})
+
+app.get("/cadastrar", function(requisicao, resposta){
+    let nome = requisicao.query.nome;
+    let login = requisicao.query.login;
+    let senha = requisicao.query.senha;
+    let nasc = requisicao.query.nascimento;
+
+    console.log(nome, login, senha, nasc)
+})
+
+app.post("/cadastrar", function(requisicao, resposta){
+    let nome = requisicao.body.nome;
+    let login = requisicao.body.login;
+    let senha = requisicao.body.senha;
+    let nasc = requisicao.body.nascimento;
+    console.log(nome, login, senha, nasc);
+
+    var data = { db_nome: nome, db_login: login, db_senha: senha, db_nasc: nasc };
+
+    usuarios.insertOne(data, function(err){
+        if(err){
+            resposta.render("resposta",{status: "Erro" ,nome, login, senha, nasc});
+        }else{
+            resposta.render("resposta",{status: "Sucesso", nome, login, senha, nasc});
+        }
+    })
+})
+
+app.get("/for_ejs",function(requisicao, resposta){
+    let valor = requisicao.query.valor;
+    resposta.render("exemplo_for",{valor});
+})
+
+app.post('/logar', function(requisicao, resposta){
+    let login = requisicao.body.login;
+    let senha = requisicao.body.senha;
+    console.log(login, senha);
+
+    var data = {db_login: login, db_senha: senha}
+
+    usuarios.find(data).toArray(function(err, items){
+        console.log(items)
+        if(items.length == 0){
+            resposta.render("resposta_login",{status: "usuario/senha não encontrado"});
+        }else if(err){
+            resposta.render("resposta_login",{status: "erro ao logar"});
+        }else{
+            resposta.render("resposta_login",{status: "usuario "+login+" logado"});
+        }
+    })
+
+})
